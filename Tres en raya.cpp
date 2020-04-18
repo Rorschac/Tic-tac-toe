cbc02e3bf2e779be0850c24b7580d5d9ca0837c3
Rorschac <63753759+Rorschac@users.noreply.github.com>
2020-04-18 20:55:01 +0100

    Add files via upload

---
+++ Tres en raya.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main (void)
+
+{
+    int raya [3][3]
+    {
+        1,2,3,
+        4,5,6,
+        7,8,9
+    };
+    int circulo,cruz,x,y;
+    char cir,cru;
+    cir='O';
+    cru='X';
+
+    printf ("\n\n");
+
+     for (x=0;x<=0;x++)
+        for (y=0;y<=2;y++)
+      printf ("\t |%d|  ",raya[x][y]);
+      printf("\n\t ===================");
+      printf("\n");
+
+       for (x=1;x<=1;x++)
+        for (y=0;y<=2;y++)
+      printf ("\t |%d|  ",raya[x][y]);
+      printf("\n\t ===================");
+       printf("\n");
+
+       for (x=2;x<=2;x++)
+        for (y=0;y<=2;y++)
+      printf ("\t |%d|  ",raya[x][y]);
+
+
+
+
+
+      printf ("\n\n Pon tu ficha:");
+
+
+
+
+
+
+
+
+    getchar ();
+
+    return 0;
+
+
+    getchar ();
+
+    return 0;
+
+}
